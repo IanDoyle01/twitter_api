0906cd25108603c49dfcda2ac8dd62c66ed3c9ab
Ian Doyle <iandoyle01@gmail.com>
2018-11-03 16:06:59 +0000

    added auth function

--- twitter_user.py
+++ twitter_user.py
@@ -1,13 +1,9 @@
 import tweepy
 from tweepy import OAuthHandler
+from twitter import get_auth, twitter_api
 
-CONSUMER_KEY = 'WzT76mRT2z1oACipf0Fa6w5fP'
-CONSUMER_SECRET = 'u0Ni4LVE4O1qhWgYfWIagMG8gcnI7arJJm8b2v56cFSW94LMRA'
-OAUTH_TOKEN = '272497238-hnCGnwFeU9xhVWlTd9AKy1gyIk6QF5Egtm9AI8Bm'
-OAUTH_TOKEN_SECRET = 'x4ZPGXwRWbKfId48uzxuSmG4BzwYLzNAmpIEijRSDq1fi'
-
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+api = twitter_api()
+auth = get_auth()
 
 # Invoke tweepy API and pass auth token
 api = tweepy.API(auth)
